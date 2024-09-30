0ce8251ca2e9ab41c92aed7ad7c648ec11fda6bd
Jerry1234530 <ficej18@plymstockschool.org.uk>
2024-09-30 08:57:00 +0000

    Added Comments

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+
+#---------IMPORTS---------# 
 import pygame, sys , os
 from settings import * 
+#---------IMPORTS---------#
 
-
-os.environ["SDL_AUDIODRIVER"] = "dsp"
+#---------INITIALISATION---------#
 pygame.init() 
 screen = pygame.display.set_mode((SCREEN_HEIGHT ,  SCREEN_WIDTH))
 clock = pygame.time.Clock() 
+#---------INITIALISATION---------#
+
+#---------Main Game Loop----------#
 run = True
 while run: 
     for event in pygame.event.get(): 
@@ -14,5 +19,5 @@ while run:
             sys.exit()
     screen.fill("BLACK") 
     pygame.display.update() 
-    clock.tick(60)  
+    clock.tick(60)  # Forces a game update to happen 60 times a second. This could cause the game to break if set to a higher refresh rate as more updates are being done.
 
