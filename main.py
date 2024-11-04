cbeaa5b0de1e06efde23bcce1eb2bee4304572dd
Jerry1234530 <ficej18@plymstockschool.org.uk>
2024-11-04 10:24:26 +0000

    Added and Updated existing files

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 
 #---------IMPORTS---------# 
 import pygame, sys , os
-from settings import * 
-#---------IMPORTS---------#
+from settings import * # Imports the variables from settings
+
 
 #---------INITIALISATION---------#
 pygame.init() 
-screen = pygame.display.set_mode((SCREEN_HEIGHT ,  SCREEN_WIDTH))
+screen = pygame.display.set_mode((SCREEN_HEIGHT,SCREEN_WIDTH))
 clock = pygame.time.Clock() 
-#---------INITIALISATION---------#
+
 
 #---------Main Game Loop----------#
 run = True
@@ -20,4 +20,3 @@ while run:
     screen.fill("BLACK") 
     pygame.display.update() 
     clock.tick(60)  # Forces a game update to happen 60 times a second. This could cause the game to break if set to a higher refresh rate as more updates are being done.
-
