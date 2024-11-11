a744267faabb74a6c4a7660bb97801c1d1daf9c5
Jerry1234530 <ficej18@plymstockschool.org.uk>
2024-11-11 09:51:13 +0000

    V0.1.2

--- Assets/tmx/Grass.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="Grass" tilewidth="32" tileheight="32" tilecount="15" columns="5">
- <image source="../Assets/Tilesets/Grass.png" width="176" height="112"/>
-</tileset>
